9d9698f20e535ab62c8ab895ab5e7960dab27790
Valoren <miquel.bernat@yahoo.es>
2020-01-05 20:23:39 +0100

    Created Menu

--- src/menu_example.cpp
+++ src/menu_example.cpp
@@ -1,33 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
- 
-#include "menu.h"
-#include "menu.cpp" /* solo si no se va crear un proyecto*/
- 
-int main(int argc, char *argv[])
+#include <iostream>
+
+using namespace std;
+
+typedef void (*Menu_Processing_Function_Pointer)(void);
+
+struct Menu_Option
 {
-    Menu menu;
-    char *opciones[] = {"Continuar","Salir",""}; /* la ultima opcion siempre es una cadena vacia*/
-    int opcion;
- 
-    inicializarMenu(&menu,"MENU DE PRUEBA","Escoger una opcion:",opciones);
- 
-    do{
-        opcion=mostrarMenu(&menu,MENU_MODO_ULTIMA_CERO);
- 
-        switch(opcion)
-        {
-            case 1:
-                printf("Has escogido continuar.\n");
-                break;
-            case 0:
-                printf("Has escogido salir.\n");
-                break;
-        }
-    }while(opcion!=0);
- 
-    /* ¡¡¡¡¡IMPORTANTE!!!!! */
-    finalizarMenu(&menu);
- 
-    return 0;
+  char choice;
+  char const * p_selection_text;
+  Menu_Processing_Function_Pointer p_processing_function;
+};
+
+void Process_Selection_One(){
+    cout << "Hello World" << endl;
+};
+void Process_Selection_Two(){
+    cout << "Hola" << endl;
+};
+
+static const Menu_Option main_menu[] =
+{
+  {'1', "Option 1", Process_Selection_One},
+  {'2', "Option 2", Process_Selection_Two},
+};
+static const size_t quantity_selections =
+    sizeof(main_menu) / sizeof(main_menu[0]);
+
+void title(){
+    cout << "-----------------------------------------\n" << endl;
+    cout << "Celestial\n" << endl;
+    cout << "A stellar dynamics toolkit\n" << endl;
+    cout << "-----------------------------------------\n" << endl;
+}
+int main()
+{
+  title();
+  for (size_t i = 0; i < quantity_selections; ++i)
+  {
+    std::cout << main_menu[i].p_selection_text << "\n";
+  }
+  cout << "Enter selection, 0 to quit: ";
+  char choice;
+  cin >> choice;
+  for (size_t i = 0; i < quantity_selections; ++i)
+  {
+     if (choice == main_menu[i].choice)
+     {
+       Menu_Processing_Function_Pointer p_function = main_menu[i].p_processing_function;
+       (p_function)();
+       break;
+     }
+  }
+  return EXIT_SUCCESS;
 }
