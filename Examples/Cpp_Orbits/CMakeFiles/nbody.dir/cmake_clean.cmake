6f885461cf080522dfc42ac7ad37fbec3235443e
Valoren <miquel.bernat@yahoo.es>
2020-03-16 19:56:52 +0100

    Example tests

---
+++ Examples/Cpp_Orbits/CMakeFiles/nbody.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nbody.dir/simulation.cpp.o"
+  "CMakeFiles/nbody.dir/orbit_integration.cpp.o"
+  "nbody.pdb"
+  "nbody"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nbody.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
